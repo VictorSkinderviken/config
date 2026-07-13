b850487cba8613880592ff3e0d613b4245c503ef
Victor Skinderviken <159253972+VictorSkinderviken@users.noreply.github.com>
2026-07-13 20:16:37 +0200

    Update monitor and workspace configurations in gentoo.lua

--- hypr/gentoo.lua
+++ hypr/gentoo.lua
@@ -2,7 +2,6 @@
 ---- MONITORS ----
 ------------------
 
--- Monitor setup home. This is for 2 monitors with the same resolution and refresh rate. If you have different monitors, you can set them up here as well.
 hl.monitor({
     output   = "DP-3",
     mode     = "2560x1440@240",
@@ -21,12 +20,9 @@ hl.monitor({
 ---- WORKSPACES PER MONITOR ----
 -----------------------------
 
--- Alt tabbing monitor 1
 hl.workspace_rule({ workspace = "1", monitor = "DP-3", default = true, persistent = true })
 hl.workspace_rule({ workspace = "2", monitor = "DP-3", persistent = true })
 hl.workspace_rule({ workspace = "3", monitor = "DP-3", persistent = true })
-
--- Alt tabbing monitor 2
 hl.workspace_rule({ workspace = "4", monitor = "DP-1", default = true, persistent = true })
 hl.workspace_rule({ workspace = "5", monitor = "DP-1", persistent = true })
 hl.workspace_rule({ workspace = "6", monitor = "DP-1", persistent = true })
@@ -35,6 +31,12 @@ hl.workspace_rule({ workspace = "6", monitor = "DP-1", persistent = true })
 ---- ALT+TAB PER MONITOR ----
 -----------------------------
 
--- Alt tab rules
-hl.bind("ALT + Tab",         hl.dsp.focus({ workspace = "m+1" }))
-hl.bind("ALT + SHIFT + Tab", hl.dsp.focus({ workspace = "m-1" }))
+hl.bind("ALT + Tab",         hl.dsp.focus({ workspace = "m-1" }))
+hl.bind("ALT + SHIFT + Tab", hl.dsp.focus({ workspace = "m+1" }))
+
+------------------------
+---- SCREENSHOT (hyprshot) ----
+------------------------
+
+hl.env("HYPRSHOT_DIR", "/home/victor/Pictures/Screenshots")
+hl.bind("SUPER + SHIFT + S", hl.dsp.exec_cmd("hyprshot -m region --clipboard-only"))
